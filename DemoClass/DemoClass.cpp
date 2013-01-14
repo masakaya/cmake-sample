5806911fc4cd957a952e19f6ebe5c689461cd7bf
Masashi Kayahara <sylphs.mb@gmail.com>
2013-01-14 15:55:29 +0900

    ref #1

--- DemoClass/DemoClass.cpp
+++ DemoClass/DemoClass.cpp
@@ -13,6 +13,6 @@ DemoClass::~DemoClass()
 }
 void DemoClass::print()
 {
-	say.setMessage("Hello Test.999\n");
+	say.setMessage("Hello Test.\n");
 	printf("DemoMessage %s\n", say.getMessage().c_str() );
 }
