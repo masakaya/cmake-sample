006424891e67f8ad3a3503ebbe2e3447db2d59ee
Masashi Kayahara <sylphs.mb@gmail.com>
2013-01-20 22:36:45 +0900

    CMakeList Fix #18

---
+++ src/Main/main.cpp
@@ -0,0 +1,35 @@
+/* Copyright (C) 
+ * 2013 - Masashi Kayahara <sylphs.mb AT gmail.com>
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+ * 
+ */
+/*!
+ * @file main.cpp
+ * @brief 
+ * @author Masashi Kayahara <sylphs.mb AT gmail.com>
+ * @version 0.0.1
+ * @date 2013-01-20
+ */
+#include <stdio.h>
+#include "DemoClass.h"
+
+int main(int argc, char const* argv[])
+{
+	DemoClass demo;
+	demo.print();
+    printf("Test.\n");
+    printf("sample\n");
+	return 0;
+}
