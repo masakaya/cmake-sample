b3f7bbcc26807fcb0571302081f44f314c8f20d9
Masashi Kayahara <sylphs.mb@gmail.com>
2013-01-21 23:47:31 +0900

    UnitTest TestAdd #20

--- test/SayClass/UnitTest_SayClass.cpp
+++ test/SayClass/UnitTest_SayClass.cpp
@@ -1,5 +1,26 @@
 #include <gtest/gtest.h>
 
+
+int add(int x, int y)
+{
+    return x + y;
+}
+
+TEST(AddTest, Test1)
+{
+    ASSERT_EQ(2, add(1, 1));
+}
+
+TEST(AddTest, Test2)
+{
+    ASSERT_EQ(3, add(1, 2));
+}
+
+TEST(AddTest, Test3)
+{
+    ASSERT_EQ(4, add(2, 1));
+}
+
 // Main 
 int main(int argc, char* argv[])
 {
